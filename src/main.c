663624783c78cd4d4ea9584accd6aacbf2e54dce
johnstef99 <john@johnstef.com>
2022-11-19 18:41:11 +0200

    update main

--- src/main.c
+++ src/main.c
@@ -1,76 +1,82 @@
 /*
  *
  * Created by:   github.com/johnstef99
- * Last updated: 2022-11-12
+ * Last updated: 2022-11-18
  *
  */
 
-#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../libs/mmio.h"
+#include "fifo.h"
 #include "graph.h"
 #include "node.h"
-#include "utils.h"
 
-void fill_graph_from_file(char *path, graph *g) {
-  FILE *file = NULL;
+graph fill_graph_from_file(char *path) {
+  int ret_code;
   MM_typecode matcode;
-
-  file = fopen(path, "r");
-  if (file == NULL) {
-    fprintf(stderr, "Couldn't open matrix file %s\n", path);
-    exit(1);
-  }
-
-  if (mm_read_banner(file, &matcode) != 0) {
-    fprintf(stderr, "Couldn't proccess matrix file\n");
-    exit(1);
-  }
-
-  if (mm_is_valid(matcode) != 1) {
-    fprintf(stderr, "Matrix not valid");
-    exit(1);
-  }
-
+  FILE *f;
   int M, N, nz;
-  int i, *I, *J;
-  int *val;
+  size_t i, *I, *J;
 
-  if ((mm_read_mtx_crd_size(file, &M, &N, &nz)) != 0)
+  if ((f = fopen(path, "r")) == NULL) {
+    fprintf(stderr, "Couldn't open file %s\n", path);
+    exit(1);
+  }
+
+  if (mm_read_banner(f, &matcode) != 0) {
+    printf("Could not process Matrix Market banner.\n");
+    exit(1);
+  }
+
+  if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &nz)) != 0)
     exit(1);
 
-  *g = graph_new(M);
-
-  I = (int *)malloc(nz * sizeof(int));
-  J = (int *)malloc(nz * sizeof(int));
-  val = (int *)malloc(nz * sizeof(int));
+  I = (size_t *)malloc(nz * sizeof(size_t));
+  J = (size_t *)malloc(nz * sizeof(size_t));
 
   for (i = 0; i < nz; i++) {
-    fscanf(file, "%d %d %d\n", &I[i], &J[i], &val[i]);
-    I[i]--; /* adjust from 1-based to 0-based */
-    J[i]--;
+    fscanf(f, "%zu %zu\n", &I[i], &J[i]);
   }
 
-  if (file != stdin)
-    fclose(file);
+  if (f != stdin)
+    fclose(f);
 
-  for (i = 0; i < nz; i++)
-    graph_add_edge(g, I[i], J[i]);
+  mm_write_banner(stdout, matcode);
+  mm_write_mtx_crd_size(stdout, M, N, nz);
+
+  graph g = graph_new(M);
+  for (i = 0; i < nz; i++) {
+    graph_add_edge(g, I[i] - 1, J[i] - 1);
+  }
+  return g;
 }
 
 int main(int argc, char *argv[]) {
+
   if (argc < 2) {
-    fprintf(stderr, "Usage: %s [directory-of-matricies]\n", argv[0]);
+    fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
     exit(1);
   }
 
-  graph g = NULL;
-  fill_graph_from_file(argv[1], &g);
+  graph g = fill_graph_from_file(argv[1]);
+  graph_trim(g);
+  printf("Trimmed: %zu vertices\n", g->n_trimmed);
+  graph_colorSCC(g);
 
-  graph_free(&g);
+  int num_of_scc = 0;
+  uint8_t *used_id = calloc(g->v, sizeof(uint8_t));
+  for (int i = 0; i < g->v; i++) {
+    if (used_id[g->scc_id[i]] == 0) {
+      used_id[g->scc_id[i]] = 1;
+      num_of_scc++;
+    }
+  }
+  free(used_id);
+
+  printf("Num of scc: %d\n", num_of_scc);
 
   return 0;
 }
